dac4cfb8044cdfd59a0bd303953fe866d75235e1
EDHidalgo <Hidalgo.Edmundo@gmail.com>
2019-05-16 11:32:19 -0500

    LRU and Program comments added

--- EECS_645_Project/LRU.cs
+++ EECS_645_Project/LRU.cs
@@ -6,9 +6,17 @@ using System.Threading.Tasks;
 
 namespace EECS_645_Project
 {
+    /* LRU models a structure used to determine
+     * the least recently used page */
     public class LRU
     {
+	/* wayNumber is a list of integers used
+	 * to store the path history of ways taken, 
+	 * pushing the LRU towards the wrong
+	 * of the list */
         List<int> wayNumber;
+
+	/* Default LRU constructor */
         public LRU(int NumberOfWays)
         {
             wayNumber = new List<int>();
@@ -18,11 +26,15 @@ namespace EECS_645_Project
             }
         }
         
+	/* GetLeastRecentlyUsedWayNumber returns the first element
+	 * in our list, which represents the LRU way number */
         public int GetLeastRecentlyUsedWayNumber()
         {
             return wayNumber[0];
         }
 
+	/* UpdateLRU replaced a wayNumber from our list
+	 * of values */
         public void UpdateLRU(int UsedWay)
         {
             wayNumber.Remove(UsedWay);
--- EECS_645_Project/Program.cs
+++ EECS_645_Project/Program.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 
 namespace EECS_645_Project
 {
+    /* Program is used to launch the simulator */
     public class Program
     {
         static void Main(string[] args)
         {
-            Computer computer = new Computer(); //Create a new Computer
+	    /* Our simulation is ran by first creating a virtual
+	     * computer and then running the simulation from within
+	     * that virtual computer */
+            Computer computer = new Computer();
             computer.RunSimulation(); //Run the Simulation      -Hint Visual Studio allows you to press F12 to go to function definition
         }
     }
