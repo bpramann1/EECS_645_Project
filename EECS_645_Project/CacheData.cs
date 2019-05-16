1ebe8830f5b5283d02f0f1e81e27de12c44b165c
EDHidalgo <Hidalgo.Edmundo@gmail.com>
2019-05-16 11:07:58 -0500

    Comments added and removed unused tag parameter from WriteData

--- EECS_645_Project/CacheData.cs
+++ EECS_645_Project/CacheData.cs
@@ -6,23 +6,28 @@ using System.Threading.Tasks;
 
 namespace EECS_645_Project
 {
+    /* CacheData holds a string representation of cache data */
     public class CacheData
     {
+	/* String object representing cache data */
         public string data;
+
         CacheWay cacheWay;
+
+	/* Default CacheData constructor */
         public CacheData(CacheWay CacheWay)
         {
             data = "";
             cacheWay = CacheWay;
         }
 
-
-
-        public void WriteData(string inputData, string inputTag)
+	/* WriteData stores new input data */
+        public void WriteData(string inputData)
         {
             data = inputData;
         }
-
+	
+	/* GetData returns the string representation of a cache's data */
         public string GetData()
         {
             return data;
