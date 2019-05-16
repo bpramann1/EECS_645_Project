061983ca99b49eb060b7bcd96aa13ba43a2a4acc
EDHidalgo <Hidalgo.Edmundo@gmail.com>
2019-05-16 10:52:20 -0500

    BusSignal comments added

--- EECS_645_Project/BusSignal.cs
+++ EECS_645_Project/BusSignal.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Namespace */
 namespace EECS_645_Project
 {
     public class BusSignal
     {
+	 /* Bus Transaction is used to represent the MOESI cache transaction */
         public BusTransactions transaction;
-        public string tag;
+
+	/* A string representation of a cache tag, index, and offset */
+        public string tag; 
         public string index;
         public string offset;
+
+	/* BusSignal is used to pass messages around that represent bus requests */
         public BusSignal(BusTransactions Transaction, string Tag = null, string Index = "", string Offset = "")
         {
             tag = Tag;
