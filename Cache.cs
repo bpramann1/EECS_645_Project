927ad9af584c4763d14a35c3db46923779c443dc
EDHidalgo <Hidalgo.Edmundo@gmail.com>
2019-05-16 11:02:17 -0500

    Cache comments added

---
+++ Cache.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EECS_645_Project
+{
+    /* Cache models a cache block */
+    public class Cache
+    {
+        /* Processor associated with this cache block */
+        public Processor controllingProcessor;
+
+	/* Array representation of CacheLines */
+        public CacheLine[] cacheLines;
+
+	/* Default Cache Constructor */
+        public Cache(Processor ControllingProcessor)
+        {
+            controllingProcessor = ControllingProcessor;
+            cacheLines = new CacheLine[(int)Math.Pow(2, 8)];
+
+            for (int i = 0; i < cacheLines.Length; i++)
+            {
+                cacheLines[i] = new CacheLine(this);
+            }
+        }
+
+	/* WriteData converts a binary numbers into its decimal version in order to find
+	 * the appropriate line index and then writes the passed data */
+        public void WriteData(string inputData, string tag, string index, string offset)
+        {
+            cacheLines[Conversions.BinaryToDecimal(index)].WriteData(inputData, tag, offset);
+        }
+
+        //public void Flush(string tag, string index, string offset)
+        //{
+        //    cacheLines[Conversions.BinaryToDecimal(index)].Flush(tag, offset);
+        //}
+
+	/* IsFlushNeeded returns a True / False value based on the MOESI protocol and
+	 * an input bus signal */
+        public bool IsFlushNeeded(BusSignal signal)
+        {
+            return cacheLines[Conversions.BinaryToDecimal(signal.index)].IsFlushNeeded(signal);
+        }
+
+	/* ChangeState updates a cacheLine based on the MOESI protocol */
+        public void ChangeState(bool DataTransactionIsInitiatedByCurrentProcessor, bool write, bool shared, string tag, string index, string offset, BusTransactions transaction = BusTransactions.ExclusiveRead)
+        {
+            cacheLines[Conversions.BinaryToDecimal(index)].ChangeState(tag, offset, DataTransactionIsInitiatedByCurrentProcessor, write, shared, transaction);
+        }
+
+	/* GetData returns the data stored in a single cacheLine */
+        public string GetData(string tag, string index, string offset)
+        {
+            return cacheLines[Conversions.BinaryToDecimal(index)].GetData(tag, offset);
+        }
+
+	/* HasData returns a bool value representing if a specific cacheLine has
+	 * data */
+        public bool HasData(string tag, string index, string offset)
+        {
+            return cacheLines[Conversions.BinaryToDecimal(index)].HasTag(tag, offset);
+        }
+
+	/* SharedData returns a bool value representing if the data in a cache line
+	 * is in the MOESI 'Shared' state */
+        public bool SharedData(string tag, string index, string offset)
+        {          
+            return cacheLines[Conversions.BinaryToDecimal(index)].SharedData(tag, offset);
+        }
+
+
+	/* ShouldSendSignal returns a bool value representing whether the MOESI protocol
+	 * requires a signal be sent */
+        public bool ShouldSendSignal(bool write, string tag, string index, string offset)
+        {
+            return cacheLines[Conversions.BinaryToDecimal(index)].ShouldSendSignal(write, tag, offset);
+        }
+	
+	/* GetSignalToBeSent returns a BusSignal based on the state of a cache line */
+        public BusSignal GetSignalToBeSent(bool write, string tag, string index, string offset)
+        {
+            return cacheLines[Conversions.BinaryToDecimal(index)].GetSignalToBeSent(write, tag, index, offset);
+        }
+    }
+}
